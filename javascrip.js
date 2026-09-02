f8a1980d39fb0ccce233ec66473987fb77f7b57f
jhoselin paloma <jhoselinpaloma.et12de1@gmail.com>
2026-09-01 21:27:41 -0300

    resultado final

--- javascrip.js
+++ javascrip.js
@@ -1,9 +1,12 @@
 function calcular(){
 
+
     var numero1 = document.getElementById("numero1");
     var numero2 = document.getElementById("numero2");
-    console.log(numero1.value)
-    var suma = numero(numero1) + numero(numero2)
-        document.getElementById("resuntado").innerText ="el resultado es.."+suma;
+
+    var multiplicacion = (numero1.value) * (numero2.value);
+    console.log(multiplicacion)
+var ressultado =document.getElementById("ressultado");
+resultado.innerText = "El resultado es: " + multiplicacion;
     
 }
